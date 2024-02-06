ed3b400046becee2a3852d9b0f1fe4dc1ee02127
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-02-06 23:14:32 +0000

    changes

--- player.py
+++ player.py
@@ -1,9 +1,18 @@
 import pygame
 
-class Player:
+
+
+
+class Player(pygame.sprite.Sprite):
     def __init__(self):
-        self.x = 370
-        self.y = 480
-        self.x_change = 0
-        self.y_change = 0
-        self.playerImg = pygame.image.load("player.png")
+        super().__init__()
+        self.image = pygame.image.load("images/player.png")
+        self.rect = self.image.get_rect()
+        self.speed = 5
+
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and self.rect.left > 0:
+            self.rect.x -= self.speed
+        if keys[pygame.K_RIGHT] and self.rect.right < 800:
+            self.rect.x += self.speed
