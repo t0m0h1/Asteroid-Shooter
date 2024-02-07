33723913577a5d817ec25b221dc571f2d2967ac4
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-02-07 20:57:15 +0000

    Update user authentication logic to use bcrypt for password hashing

---
+++ asteroid.py
@@ -0,0 +1,18 @@
+import pygame
+import random
+
+class Asteroid(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("images/asteroid.png")
+        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.rect = self.image.get_rect()
+        self.speed = 4
+        self.rect.x = random.randint(0, 800)
+        self.rect.y = random.randint(-100, -40)
+
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.top > 600:
+            self.rect.x = random.randint(0, 800)
+            self.rect.y = random.randint(-100, -40)
