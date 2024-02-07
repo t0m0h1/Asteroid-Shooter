ec3670807f0ce7f232b9386b8b15f2ad6f75cc37
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-02-07 20:54:30 +0000

    Adjust laser size and color

--- laser.py
+++ laser.py
@@ -6,8 +6,8 @@ import pygame
 class Laser(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.Surface((3, 5))  # Adjust the size of the laser
-        self.image.fill((0, 0, 255))  # Fill the laser surface with a red color
+        self.image = pygame.Surface((5, 3))  # Adjust the size of the laser
+        self.image.fill((0, 255, 255))  # Fill the laser surface with a red color
         self.rect = self.image.get_rect()
         self.rect.midbottom = (x, y)
         self.speed = 4
